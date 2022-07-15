911971ce87551e03e80dca0bf3efb7ee30e6f936
ua-rexdeep <ua.rexdeep@gmail.com>
2022-07-15 13:38:33 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,41 +1,89 @@
-/**
- * This class is just a facade for your implementation, the tests below are using the `World` class only.
- * Feel free to add the data and behavior, but don't change the public interface.
- */
+class HouseHold {
+  constructor(id){
+    this.id = id;
+    this.connectedToPowerPlants = [];
+    this.connectedHouseHolds = [];
+  }
 
+  addConnectedHouseHold(houseHold){
+    this.connectedHouseHolds.push(houseHold);
+  }
+
+  connectToPowerPlant(powerPlant){
+    this.connectedToPowerPlants.push(powerPlant);
+  }
+
+  disconnectFromPowerPlant(powerPlant){
+    this.connectedToPowerPlants.splice(this.connectedToPowerPlants.indexOf(powerPlant), 1);
+  }
+
+  isHouseholdPoweredFromPowerPlants(){
+    return this.connectedToPowerPlants.some(powerPlant => powerPlant.alive);
+  }
+
+  // було очікувано, що я впаду в інфініт, тримай кавун :)
+  isHouseholdPoweredFromHouseholds(duplicator){
+    const searchArr = this.connectedHouseHolds.filter(x=>!duplicator.has(x.id));
+    return searchArr.some(houseHold => houseHold.isHouseholdPowered(duplicator));
+  }
+
+  // зробив змінну сетом, щоб запобігти падінню в інфініт, та з'єкономити на перевірках ідентичних id
+  isHouseholdPowered(duplicator = new Set()){
+    duplicator.add(this.id);
+    return this.isHouseholdPoweredFromPowerPlants() || this.isHouseholdPoweredFromHouseholds(duplicator);
+  }
+}
+
+class PowerPlant {
+  constructor() {
+    this.alive = true;
+  }
+
+
+  kill() {
+    this.alive = false;
+  }
+
+  repair() {
+    this.alive = true;
+  }
+}
 
 export class World {
-  constructor() {}
+  constructor() {
+    this.houseHoldNextId = 0;
+  }
 
   createPowerPlant() {
-    throw new Error("Not Implemented");
+    return new PowerPlant();
   }
 
   createHousehold() {
-    throw new Error("Not Implemented");
+    return new HouseHold(this.houseHoldNextId++)
   }
 
   connectHouseholdToPowerPlant(household, powerPlant) {
-    throw new Error("Not Implemented");
+    household.connectToPowerPlant(powerPlant);
   }
 
   connectHouseholdToHousehold(household1, household2) {
-    throw new Error("Not Implemented");
+    household1.addConnectedHouseHold(household2);
+    household2.addConnectedHouseHold(household1);
   }
 
   disconnectHouseholdFromPowerPlant(household, powerPlant) {
-    throw new Error("Not Implemented");
+    household.disconnectFromPowerPlant(powerPlant);
   }
 
   killPowerPlant(powerPlant) {
-    throw new Error("Not Implemented");
+    powerPlant.kill();
   }
 
   repairPowerPlant(powerPlant) {
-    throw new Error("Not Implemented");
+    powerPlant.repair()
   }
 
   householdHasEletricity(household) {
-    throw new Error("Not Implemented");
+    return household.isHouseholdPowered();
   }
 }
